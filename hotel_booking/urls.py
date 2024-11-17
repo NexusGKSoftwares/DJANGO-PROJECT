8458b700321afe1598ffcd6070ba39a90a55a98f
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-17 11:39:55 +0300

    "Added new URL pattern for index view at root URL"

--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('rooms/', views.available_rooms, name='available_rooms'),
     path('book/<int:room_id>/', views.book_room, name='book_room'),
     path('booking_success/<int:booking_id>/', views.booking_success, name='booking_success'),
