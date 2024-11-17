a59b6428795bd4ed7b8c75599db5e4251602427a
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-17 12:25:38 +0300

    "Added import statements for settings and static URLs, and appended static URL configuration to urlpatterns."

--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -1,6 +1,8 @@
 # hotel_booking/urls.py
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('book/<int:room_id>/', views.book_room, name='book_room'),
     path('booking_success/<int:booking_id>/', views.booking_success, name='booking_success'),
     path('add-room/', views.add_room, name='add_room'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
