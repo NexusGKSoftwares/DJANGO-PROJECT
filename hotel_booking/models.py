16c3614813a766149b4a0063ccecb54e45656789
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-17 12:23:36 +0300

    "Added image field to Room model"

--- hotel_booking/models.py
+++ hotel_booking/models.py
@@ -4,6 +4,7 @@ class Room(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='room_images/', blank=True, null=True)  # Added image field
 
     def __str__(self):
         return self.name
