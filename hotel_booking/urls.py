7c8cf06c57264ded0a54fee8f67e5eb0c3359250
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-17 13:40:17 +0300

    "Added new URL pattern for room details page"

--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('book/<int:room_id>/', views.book_room, name='book_room'),
     path('booking_success/<int:booking_id>/', views.booking_success, name='booking_success'),
     path('add-room/', views.add_room, name='add_room'),
+    path('room/<int:room_id>/', views.room_details, name='room_details'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
