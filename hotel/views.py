c11bd5ba4e58bcc0c6536e622df2b17598724ef6
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-17 11:08:50 +0300

    "Added available_rooms and book_room views, imported necessary models and forms, and implemented booking logic."

--- hotel/views.py
+++ hotel/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Room, Booking
+from .forms import BookingForm
+
+def available_rooms(request):
+    rooms = Room.objects.filter(availability=True)
+    return render(request, 'hotel_booking/available_rooms.html', {'rooms': rooms})
+
+def book_room(request, room_id):
+    room = get_object_or_404(Room, pk=room_id)
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.room = room
+            booking.total_price = room.price_per_night * (booking.check_out_date - booking.check_in_date).days
+            booking.save()
+            return redirect('booking_success')
+    else:
+        form = BookingForm()
+    return render(request, 'book_room.html', {'form': form, 'room': room})
