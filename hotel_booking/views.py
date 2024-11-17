a6f1bd6219b7a55e4046964f26a3846b9e2d4496
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-17 11:34:56 +0300

    "Updated import order in views.py, modified book_room view to pass booking_id to redirect, and added new booking_success view and template."

--- hotel_booking/views.py
+++ hotel_booking/views.py
@@ -1,5 +1,5 @@
 # hotel_booking/views.py
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Room, Booking
 from .forms import BookingForm
 
@@ -16,7 +16,11 @@ def book_room(request, room_id):
             booking.room = room
             booking.total_price = room.price_per_night * (booking.check_out_date - booking.check_in_date).days
             booking.save()
-            return redirect('booking_success')
+            return redirect('booking_success', booking_id=booking.id)
     else:
         form = BookingForm()
     return render(request, 'hotel_booking/book_room.html', {'form': form, 'room': room})
+
+def booking_success(request, booking_id):
+    booking = get_object_or_404(Booking, pk=booking_id)
+    return render(request, 'hotel_booking/booking_success.html', {'booking': booking})
