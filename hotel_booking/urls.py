a7d5b7ff02b27617b88644390758c6c239b0e018
NexusGKSoftwares <nexusgk3@gmail.com>
2024-11-18 09:07:26 +0300

    "Added conditional media URL pattern to urlpatterns in hotel_booking/urls.py"

--- hotel_booking/urls.py
+++ hotel_booking/urls.py
@@ -21,3 +21,6 @@ urlpatterns = [
     path('bookings/approve/<int:booking_id>/', views.approve_booking, name='approve_booking'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
